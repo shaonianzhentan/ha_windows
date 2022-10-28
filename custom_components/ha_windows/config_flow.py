2271fb997602df3b29ba761d02ad3e86167e7e38
shaonianzhetnan <5294022+shaonianzhentan@users.noreply.github.com>
2022-10-29 01:17:35 +0800

    update

--- custom_components/ha_windows/config_flow.py
+++ custom_components/ha_windows/config_flow.py
@@ -32,6 +32,6 @@ class SimpleConfigFlow(ConfigFlow, domain=DOMAIN):
             if self.hass.data.get(user_input.get('dev_id')) is not None:
                 return self.async_abort(reason="single_instance_allowed")
 
-            return self.async_create_entry(title=DOMAIN, data=user_input)
+            return self.async_create_entry(title=user_input['name'], data=user_input)
 
         return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors=errors)
